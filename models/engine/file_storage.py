d424e02bab8e331ec3237db362aa1ad2fef0fb2a
Mohamed KADI <handskadi@gmail.com>
2023-10-12 16:53:33 +0000

    File Storage - Init

---
+++ models/engine/file_storage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import datetime
+import json
+
+class FileStorage:
+    def __init__(self):
+        __file_path = "file.json"
+        __objects = {}
+
+
+    def all(self):
+        pass
+    def new(self, obj):
+        pass
+    def save(self):
+        pass
+    def reload(self):
+        pass
+
