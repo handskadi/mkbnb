15de06a9e936f34704ae0abf0a61b31338f60672
Mohamed KADI <handskadi@gmail.com>
2023-10-13 01:11:55 +0000

    File Storage handling

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@
 
 import  uuid
 from  datetime import datetime
+from models import storage
 
 class BaseModel:
     """
@@ -32,6 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ string representation of an instance"""
@@ -40,6 +42,7 @@ class BaseModel:
     def save(self):
         """ updating public instance of updated at """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ prepares a dic represenation of the instance """
@@ -49,5 +52,3 @@ class BaseModel:
         dic["updated_at"] = dic["updated_at"].isoformat()
 
         return dic
-
-
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
-
+""" File Storage Class Module """
 import datetime
 import json
+import os
 
 class FileStorage:
-    def __init__(self):
-        __file_path = "file.json"
-        __objects = {}
+
+    """ Storage class to get data """
+    __file_path = "file.json"
+    __objects = {}
 
 
     def all(self):
+        """ This returns _objects dictionary """
         return FileStorage.__objects
 
     def new(self, obj):
-        pass
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
     def save(self):
-        pass
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            mydic = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+            json.dump(mydic, f)
+
+    def classes(self):
+        from models.base_model import BaseModel
+        classes = {"BaseModel": BaseModel}
+        return classes
+
     def reload(self):
-        pass
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_d = json.load(f)
+            obj_d = {key: self.classes()[value["__class__"]](**value)
+                        for key, value in obj_d.items()}
+            FileStorage.__objects = obj_d
 
