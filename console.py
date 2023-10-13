3c08916b3fa6f9aff6f3404cc7d3b23d7c86e7bb
Mohamed KADI <handskadi@gmail.com>
2023-10-13 16:53:16 +0000

    Pullrequest file via terminal

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 import json
+import re
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(mkbnb) "
@@ -17,6 +18,19 @@ class HBNBCommand(cmd.Cmd):
         print() # to handle newline 
         return True
 
+    def emptyline(self):
+        """ Do nothing on Empty line """
+        pass
+
+    def do_create(self, cln):
+        if cln == "" or cln is None:
+            print("** class name missing **")
+        elif cln not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            new_instance = storage.classes()[cln]()
+            new_instance.save()
+            print(new_instance.id)
 
 if __name__ == '__main__':
         HBNBCommand().cmdloop()
